345bf8dd9c5ab80cd2fadc862017235713424e26
Mako <mako@mc-air.local>
2023-01-19 19:20:18 +0400

    adjustment

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const popup = document.querySelector(".popup-button");
+const popup = document.querySelector(".popup");
 const close = document.querySelector(".close-button");
+const burger = document.querySelector(".burger-content")
+const buttonImage = document.querySelector(".burger img")
 function openPopup() {
   popup.classList.remove("close-popup");
   popup.classList.add("open-popup");
@@ -7,3 +9,16 @@ function openPopup() {
 function closePopup() {
   popup.classList.add("close-popup");
 }
+
+function showCloseBurger(){
+  if(!burger.classList.contains("burger-open")){
+    burger.classList.remove("burger-close")
+    burger.classList.add("burger-open")
+    buttonImage.setAttribute("src", "./images/icon_close.png")
+  }else{
+    burger.classList.remove("burger-open")
+    burger.classList.add("burger-close")
+    buttonImage.setAttribute("src", "./images/burger.svg")
+  }
+}
+
